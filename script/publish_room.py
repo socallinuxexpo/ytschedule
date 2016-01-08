7fcc9fd53dafac9bc1b3471a4fe7c1cb0ac6c5a6
Michael Proctor-Smith <mproctor13@gmail.com>
2016-01-08 03:39:19 -0800

    first pass at pushing live streams and broadcasts.

---
+++ script/publish_room.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import time,datetime
+import pytz
+import iso8601
+from xml.etree.ElementTree import Element, SubElement, dump, parse, tostring, fromstring
+import os, sys
+import django
+from daemon import Daemon
+
+rundir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../run'))
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+sys.path.append(path)
+os.environ["DJANGO_SETTINGS_MODULE"] = "ytschedule.settings"
+
+
+TIME_ZONE ='America/Los_Angeles'
+from room.models import *
+
+tz = pytz.timezone(TIME_ZONE)
+time.tzset()
+os.environ['TZ'] = 'America/Los_Angeles'
+if __name__ == "__main__":
+  django.setup()
+  room = Room.objects.first()
+  print room.name
+
+  print room.state
+
+  room.update_description()
+  room.create_stream()
+  room.publish()
+
+  talks = Talk.objects.filter(room=room)
+  for talk in talks:
+    talk.publish()
+
