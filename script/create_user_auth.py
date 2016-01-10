a6c32910aadf094ca4ff138e52a5489239bcfa21
Michael Proctor-Smith <mproctor13@gmail.com>
2016-01-10 13:50:49 -0800

    init user oauth script

---
+++ script/create_user_auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import time,datetime
+import pytz
+import iso8601
+from xml.etree.ElementTree import Element, SubElement, dump, parse, tostring, fromstring
+import os, sys
+import django
+import argparse
+from daemon import Daemon
+from oauth2client.tools import argparser
+
+rundir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../run'))
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+sys.path.append(path)
+os.environ["DJANGO_SETTINGS_MODULE"] = "ytschedule.settings"
+
+
+TIME_ZONE ='America/Los_Angeles'
+from room.models import *
+
+tz = pytz.timezone(TIME_ZONE)
+time.tzset()
+os.environ['TZ'] = 'America/Los_Angeles'
+if __name__ == "__main__":
+  django.setup()
+  logger = logging.getLogger()
+  logger.setLevel(logging.DEBUG)
+
+  ch = logging.StreamHandler(sys.stdout)
+  ch.setLevel(logging.DEBUG)
+  formatter = logging.Formatter('%(levelname)s - %(message)s')
+  ch.setFormatter(formatter)
+  logger.addHandler(ch)
+ 
+  print YouTube.get_authenticated_service(True)
+
