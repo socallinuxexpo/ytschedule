c791407343a2983ad234dc8df9c9ae977cbac763
Michael Proctor-Smith <mproctor13@gmail.com>
2016-01-12 02:38:12 -0800

    update publish to support multiple rooms.

--- script/publish_room.py
+++ script/publish_room.py
@@ -24,26 +24,30 @@ os.environ['TZ'] = 'America/Los_Angeles'
 if __name__ == "__main__":
   django.setup()
   logger = logging.getLogger()
-  logger.setLevel(logging.DEBUG)
+  logger.setLevel(logging.WARNING)
 
   ch = logging.StreamHandler(sys.stdout)
-  ch.setLevel(logging.DEBUG)
+  ch.setLevel(logging.WARNING)
   formatter = logging.Formatter('%(levelname)s - %(message)s')
   ch.setFormatter(formatter)
   logger.addHandler(ch)
   
+  rooms = []
   if len(sys.argv) > 1:
-    room = Room.objects.get(id=sys.argv[1])
+    if sys.argv[1].lower() == "all":
+      rooms = Room.objects.all() 
+    else:
+      rooms.append( Room.objects.get(id=sys.argv[1]) )
   else:
-    room = Room.objects.first()
-  print room.title
-  print room.state
-  
-  room.update_description()
-  room.create_stream()
-  room.publish()
+    rooms.append(Room.objects.first())
 
-  talks = Talk.objects.filter(room=room)
-  for talk in talks:
-    talk.publish()
+  for room in rooms:
+    #print "[%i]%s -- %s" % (room.id, room.title, room.state)
+    room.update_description()
+    room.create_stream()
+    room.publish()
+
+    talks = Talk.objects.filter(room=room)
+    for talk in talks:
+      talk.publish()
 
