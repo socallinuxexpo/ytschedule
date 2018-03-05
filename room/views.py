eb2b459911e3b74b4ffa560d628072b5bcac2dac
Michael Proctor-Smith <mproctor13@gmail.com>
2018-03-05 01:31:21 -0800

    add index template

--- room/views.py
+++ room/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Room
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    context = { }
+    return HttpResponse(template.render(context, request))
