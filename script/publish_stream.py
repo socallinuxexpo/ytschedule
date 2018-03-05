92f3b094243750d6e14d14d625f5e1d9f9378218
Michael Proctor-Smith <mproctor13@gmail.com>
2018-03-05 01:34:12 -0800

    script to just publist stream

---
+++ script/publish_stream.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import time,datetime
+import pytz
+import iso8601
+from xml.etree.ElementTree import Element, SubElement, dump, parse, tostring, fromstring
+import os, sys
+import django
+from daemon import Daemon
+
+rundir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../run'))
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+sys.path.append(path)
+os.environ["DJANGO_SETTINGS_MODULE"] = "ytschedule.settings.production"
+
+TIME_ZONE ='America/Los_Angeles'
+from room.models import *
+
+tz = pytz.timezone(TIME_ZONE)
+time.tzset()
+os.environ['TZ'] = 'America/Los_Angeles'
+if __name__ == "__main__":
+  django.setup()
+  logger = logging.getLogger()
+  logger.setLevel(logging.WARNING)
+
+  ch = logging.StreamHandler(sys.stdout)
+  ch.setLevel(logging.WARNING)
+  logger.setLevel(logging.WARNING)
+  logger.setLevel(logging.INFO)
+  formatter = logging.Formatter('%(levelname)s - %(message)s')
+  ch.setFormatter(formatter)
+  logger.addHandler(ch)
+  
+  rooms = []
+  if len(sys.argv) > 1:
+    if sys.argv[1].lower() == "all":
+      rooms = Room.objects.all() 
+    else:
+      rooms.append( Room.objects.get(id=sys.argv[1]) )
+  else:
+    rooms.append(Room.objects.first())
+
+  for room in rooms:
+    #print "[%i]%s -- %s" % (room.id, room.title, room.state)
+    room.update_description()
+    room.create_stream()
+    room.publish()
+
+#    talks = Talk.objects.filter(room=room)
+#    for talk in talks:
+#      talk.publish()
+
