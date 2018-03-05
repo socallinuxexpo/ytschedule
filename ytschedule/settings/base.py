fc44a6dc741c8ebe0724f8026a8cd0fed3791a46
Michael Proctor-Smith <mproctor13@gmail.com>
2018-03-05 01:29:32 -0800

    updated modules.

--- ytschedule/settings/base.py
+++ ytschedule/settings/base.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'room'
+    'room',
+    'bootstrap3',
+    'tastypie',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -50,6 +52,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
+
+TASTYPIE_DEFAULT_FORMATS = ['json']
 
 ROOT_URLCONF = 'ytschedule.urls'
 
@@ -101,3 +105,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'TIMEOUT': 60
+    },
+    'resources': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'TIMEOUT': 60
+    }
+}
