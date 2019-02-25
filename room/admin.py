46b8932031e102a0e98b52e68c7e6886fe1b291f
Michael Proctor-Smith <mproctor13@gmail.com>
2019-02-25 02:09:56 -0800

    formating

--- room/admin.py
+++ room/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-
-from django.contrib import admin
-
 from .models import Room
 from .models import Talk
 from .models import CommonDescription
@@ -9,4 +6,3 @@ from .models import CommonDescription
 admin.site.register(Room)
 admin.site.register(Talk)
 admin.site.register(CommonDescription)
-
